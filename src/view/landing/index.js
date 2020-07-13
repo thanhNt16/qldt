ebb2073beda70ba8c2cf4c727794a14cf0e1a02c
duchm <duchm@cystack.net>
2020-07-13 17:58:28 +0700

    update

--- src/view/landing/index.js
+++ src/view/landing/index.js
@@ -8,6 +8,19 @@ export default function Landing() {
   return (
     <Layout className="background-white">
       <Header />
+      <div className="flex w-full">
+        <picture>
+          <source
+            srcSet={require("images/hust-banner-landing.jpg?webp")}
+            type="image/webp"
+          />
+          <source srcSet={require("images/hust-banner-landing.jpg")} type="image/jpeg" />
+          <img
+            className="w-full"
+            src={require("images/hust-banner-landing.jpg?webp")}
+          />
+        </picture>
+      </div>
     </Layout>
   );
 }
